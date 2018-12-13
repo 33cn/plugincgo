c19436a906a665de8f98a6213b20b9b049ff3900
kingwang <kingwang@kingwangdeiMac.local>
2018-12-13 16:21:08 +0800

    fix linter error

--- cli/main.go
+++ cli/main.go
@@ -8,10 +8,10 @@ package main
 
 import (
 	_ "github.com/33cn/chain33/system"
-	"github.com/33cn/plugin/cli/buildflags"
-	_ "github.com/33cn/plugin/plugin"
+	_ "github.com/33cn/plugincgo/plugin"
 
 	"github.com/33cn/chain33/util/cli"
+	"github.com/33cn/plugincgo/cli/buildflags"
 )
 
 func main() {
