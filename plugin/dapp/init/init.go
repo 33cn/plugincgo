19698d59b0b21f596a126d4b8b1325aadd3aeb4b
mdj33 <mdj33@disanbo.com>
2021-09-24 11:38:00 +0800

    remove jvm compiler due to no windows' static lib

--- plugin/dapp/init/init.go
+++ plugin/dapp/init/init.go
@@ -1,6 +1,5 @@
 package init
 
 import (
-	_ "github.com/33cn/plugin/plugin/dapp/init"   //auto gen
-	_ "github.com/33cn/plugincgo/plugin/dapp/jvm" //auto gen
+	_ "github.com/33cn/plugin/plugin/dapp/init" //auto gen
 )
